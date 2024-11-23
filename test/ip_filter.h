a4cb27d123edfedc6379042d03f0a2c87ad74d0a
Alex <ads53@tpu.ru>
2024-11-23 08:21:45 +0300

    16

--- test/ip_filter.h
+++ test/ip_filter.h
@@ -6,19 +6,19 @@
 #include <vector>
 #include <algorithm>
 
-// Функция разделения строки на части по заданному разделителю
+//разделение на части
 std::vector<std::string> split(const std::string& str, char d);
 
-// Функция для обратной лексикографической сортировки IP-адресов
+//сортировка
 void sort_ip_pool(std::vector<std::vector<std::string>>& ip_pool);
 
-// Функция фильтрации по первому байту
+//первому байту
 std::vector<std::vector<std::string>> filter_by_first_byte(const std::vector<std::vector<std::string>>& ip_pool, int byte);
 
-// Функция фильтрации по первому и второму байтам
+//второму байтам
 std::vector<std::vector<std::string>> filter_by_first_and_second_bytes(const std::vector<std::vector<std::string>>& ip_pool, int byte1, int byte2);
 
-// Функция фильтрации по любому байту
+//любой байт
 std::vector<std::vector<std::string>> filter_any_byte(const std::vector<std::vector<std::string>>& ip_pool, int byte);
 
 #endif // IP_FILTER_H
